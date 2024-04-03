104d4b451dac4e76c82eb340d3c9f3c07246f384
AnchitaSajeev <104670404+AnchitaSajeev@users.noreply.github.com>
2024-04-03 21:21:36 +0530

    Multiple destination + QrCode Added

--- IRouteFP/Assets/Scripts/SetNavigationTarget.cs
+++ IRouteFP/Assets/Scripts/SetNavigationTarget.cs
@@ -1,38 +1,54 @@
-using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UIElements;
 
 public class SetNavigationTarget : MonoBehaviour
 {
+
     [SerializeField]
-    private Camera topDownCamera;
+    private TMP_Dropdown navigationTargetDropDown;
     [SerializeField]
-    private GameObject navTargetObject;
+    private List<Target> navigationTargetObjects = new List<Target>();
 
     private NavMeshPath path;
     private LineRenderer line;
-
+    private Vector3 targetPosition = Vector3.zero;
     private bool lineToggle = false;
+
     private void Start()
     {
         path = new NavMeshPath();
         line = transform.GetComponent<LineRenderer>();
+        line.enabled = lineToggle;
     }
 
     private void Update()
     {
-        if ((Input.touchCount > 0) && (Input.GetTouch(0).phase == TouchPhase.Began))
+        if (lineToggle && targetPosition != Vector3.zero)
         {
-            lineToggle = !lineToggle;
-        }
-
-        if (lineToggle)
-        {
-            NavMesh.CalculatePath(transform.position, navTargetObject.transform.position, NavMesh.AllAreas, path);
+            NavMesh.CalculatePath(transform.position, targetPosition, NavMesh.AllAreas, path);
             line.positionCount = path.corners.Length;
             line.SetPositions(path.corners);
-            line.enabled = true;
         }
+    }
+
+
+    public void SetCurrentNavigationTarget(int selectedValue)
+    {
+        targetPosition = Vector3.zero;
+        string selectedText = navigationTargetDropDown.options[selectedValue].text;
+        Target currentTarget = navigationTargetObjects.Find(x => x.Name.Equals(selectedText));
+        if (currentTarget != null)
+        {
+            targetPosition = currentTarget.PositionObject.transform.position;
+        }
+    }
+
+    public void ToogleVisibility()
+    {
+        lineToggle = !lineToggle;
+        line.enabled = lineToggle;
     }
 }
---
+++ IRouteFP/Assets/Scripts/Target.cs
@@ -0,0 +1,11 @@
+using System;
+using UnityEngine;
+
+[Serializable]
+public class Target
+{
+    public string Name;
+    public GameObject PositionObject;
+
+}
+    
